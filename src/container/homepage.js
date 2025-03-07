f84163b36282332471dfed2eef81c2dc1f31ed0a
shantanu-1404 <avhadenterprisespc1@gmail.com>
2025-03-07 18:53:05 +0530

    d

--- src/container/homepage.js
+++ src/container/homepage.js
@@ -29,7 +29,7 @@ const Homepage = () => {
   return (
     <Layout>
       <div className="container">
-        <Row className="metrix-container">
+        <div className="row">
           {/* Total Revenue */}
 
           <Col xs={4} md>
@@ -86,7 +86,7 @@ const Homepage = () => {
               </div>
             </MetricCard>
           </Col>
-        </div>
+        </Row>
 
         {/* Announcements Section */}
         <div className="row">
